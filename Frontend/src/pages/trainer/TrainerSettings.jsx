b4c12805569b6b92edf0e2e012aaa069666fe2b3
prashiv.g04 <prashivgoyal1504@gmail.com>
2026-01-17 00:45:07 +0530

    trainer's setting and support done

--- Frontend/src/pages/trainer/TrainerSettings.jsx
+++ Frontend/src/pages/trainer/TrainerSettings.jsx
@@ -1,9 +1,220 @@
 import React from 'react'
+import navjot from '../../media/navjotImg.jpeg'
 
 const TrainerSettings = () => {
   return (
-    <div>
-      Trainer Settings Page
+    <div className=''>
+      <div className='flex-1 overflow-y-auto p-8 scroll-smooth'>
+        <div className='max-w-300 mx-auto space-y-8 pb-10'>
+          {/* Heading */}
+          <div className='flex items-center justify-between gap-4'>
+            <div className='flex flex-col items-start justify-center gap-2'>
+              <h1 className='text-4xl font-black tracking-tight'>Account Settings</h1>
+              <p className='text-[#61896f] text-base'>Manage your personal details, preferences, and account security.</p>
+            </div>
+            <div className='flex items-center gap-4'>
+              <button className='px-5 py-2.5 bg-white rounded-lg hover:bg-gray-50 font-medium border border-[#dbe6df] transition-all'>Discard</button>
+              <button className='flex items-center gap-2 px-5 py-2.5 bg-[#15ec5b] rounded-lg hover:bg-green-500 font-bold shadow-lg shadow-[#15ec5b]/25 transition-all'>
+                <i class="fa-regular fa-floppy-disk text-[20px]"></i>
+                Save Changes
+              </button>
+            </div>
+          </div>
+
+          {/* Profile Header Card */}
+          <div className='bg-white rounded-2xl border border-[#dbe6df] shadow-sm p-8 flex gap-6 items-center'>
+            <div className='group'>
+              <img className='size-32 bg-cover bg-center rounded-full border-4 border-white shadow-md object-cover' src={navjot} alt="Profile" />
+            </div>
+            <div className='flex-1 text-left space-y-2'>
+              <div className='flex items-center gap-4'>
+                <h2 className='text-2xl font-bold'>Alex Johnson</h2>
+                <span className='inline-flex items-center px-2 py-0.5 bg-blue-50 border border-blue-200 rounded-md text-sm font-bold text-blue-700'>Trainer</span>
+              </div>
+              <p className='text-[#61896f] max-w-xl'>Certified Personal Trainer specializing in HIIT and Strength Training. Helping members achieve their fitness goals since 2018.</p>
+              <div className='flex flex-wrap justify-start items-center gap-4 mt-4'>
+                <span className='inline-flex items-center gap-1 px-3 py-1 rounded-full bg-slate-100 text-slate-600 text-sm font-medium'>
+                  <i class="ri-map-pin-2-line text-[16px]"></i>
+                  San Francisco, CA
+                </span>
+                <span className='inline-flex items-center gap-1 px-3 py-1 rounded-full bg-slate-100 text-slate-600 text-sm font-medium'>
+                  <i class="ri-time-line text-[16px]"></i>
+                  Joined Sep 2018
+                </span>
+              </div>
+            </div>
+          </div>
+
+          {/* Settings Sections */}
+          <div className='grid grid-cols-3 gap-6'>
+            {/* Left */}
+            <div className='col-span-2 flex flex-col gap-6'>
+              {/* Personal Information */}
+              <section className='bg-white rounded-xl border border-[#dbe6df] shadow-sm overflow-hidden'>
+                <div className='p-8'>
+                  <div className='flex flex-col items-start gap-8'>
+                    {/* Heading */}
+                    <div className='flex items-center justify-between border-b border-[#dbe6df] w-full pb-4'>
+                      <h3 className='text-lg font-bold flex items-center gap-3'>
+                        <i className="ri-user-line text-[20px] text-[#15ec5b]"></i> 
+                        Personal Information
+                      </h3>
+                      <button className='text-sm font-semibold text-[#15ec5b] transition hover:underline'>Edit</button>
+                    </div>
+
+                    {/* Info Form */}
+                    <div className='flex-1 w-full grid grid-cols-2 gap-6'>
+                      <div className='flex flex-col gap-1.5'>
+                        <label className='text-sm font-medium'>First Name</label>
+                        <input className='w-full h-12 px-3 rounded-lg border border-[#dbe6df] bg-white focus:outline-0 focus:ring-2 focus:ring-[#15ec5b] focus:border-transparent text-sm transition-all placeholder:text-lg' type="text" placeholder='Enter full name' value='Alex' />
+                      </div>
+
+                      <div className='flex flex-col gap-1.5'>
+                        <label className='text-sm font-medium'>Last Name</label>
+                        <input className='w-full h-12 px-3 rounded-lg border border-[#dbe6df] bg-white focus:outline-0 focus:ring-2 focus:ring-[#15ec5b] focus:border-transparent text-sm transition-all placeholder:text-lg' type="text" placeholder='Enter full name' value='Johnson' />
+                      </div>
+
+                      <div className='col-span-2 flex flex-col gap-1.5'>
+                        <label className='text-sm font-medium'>Email Address</label>
+                        <div className='relative'>
+                          <i class="fa-regular fa-envelope absolute left-3 top-1/2 transform -translate-y-1/2 text-[#61896f] text-[20px]"></i>
+                          <input className='w-full h-12 pl-12 pr-3 rounded-lg border border-[#dbe6df] bg-white focus:outline-0 focus:ring-2 focus:ring-[#15ec5b] focus:border-transparent text-sm transition-all placeholder:text-lg' type="email" placeholder='name@example.com' value='alex.j@fitpulse.com' />
+                        </div>
+                      </div>
+                      
+                      <div className='flex flex-col gap-1.5'>
+                        <label className='text-sm font-medium'>Phone Number</label>
+                        <input className='w-full h-12 px-3 rounded-lg border border-[#dbe6df] bg-white focus:outline-0 focus:ring-2 focus:ring-[#15ec5b] focus:border-transparent text-sm transition-all placeholder:text-lg' type="tel" value='+1 (555) 123-4567' />
+                      </div>
+
+                      <div className='flex flex-col gap-1.5'>
+                        <label className='text-sm font-medium'>Date of Birth</label>
+                        <input className='w-full h-12 px-3 rounded-lg border border-[#dbe6df] bg-white focus:outline-0 focus:ring-2 focus:ring-[#15ec5b] focus:border-transparent text-sm transition-all placeholder:text-lg' type="date" value='1990-05-15'  />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+
+              {/* Trainer Details */}
+              <section className='bg-white rounded-xl border border-[#dbe6df] shadow-sm overflow-hidden'>
+                <div className='p-8'>
+                  <div className='flex flex-col items-start gap-8'>
+                    {/* Heading */}
+                    <div className='flex items-center justify-between border-b border-[#dbe6df] w-full pb-4'>
+                      <h3 className='text-lg font-bold flex items-center gap-3'>
+                        <i className="fa-solid fa-dumbbell text-[20px] text-[#15ec5b]"></i> 
+                        Trainer Details
+                      </h3>
+                      <button className='text-sm font-semibold text-[#15ec5b] transition hover:underline'>Edit</button>
+                    </div>
+
+                    {/* Info Form */}
+                    <div className='space-y-6 w-full'>
+                      <div className='flex flex-col gap-1.5'>
+                        <label className='text-sm font-medium'>Professional Bio</label>
+                        <textarea className='w-full px-4 py-3 rounded-lg border border-[#dbe6df] bg-slate-50 focus:outline-0 focus:ring-2 focus:ring-[#15ec5b] focus:border-transparent text-sm transition-all resize-none' rows='4'>Certified Personal Trainer specializing in HIIT and Strength Training. Passionate about helping clients build sustainable healthy habits.</textarea>
+                        <p className='text-xs text-[#61896f] text-right'>240/500 characters</p>
+                      </div>
+
+                      <div className='flex flex-col gap-1.5'>
+                        <label className='text-sm font-medium'>Specialties</label>
+                        <div className='flex flex-wrap gap-2'>
+                          <div className='flex items-center gap-1 px-3 py-1.5 bg-[#15ec5b]/10 border border-[#15ec5b]/30 rounded-md text-sm font-medium group cursor-pointer hover:bg-[#15ec5b]/30 transition-colors'>
+                            HIIT
+                            <button className='text-[#61896f] hover:text-red-500'>
+                              <i class="ri-close-fill text-[16px]"></i>
+                            </button>
+                          </div>
+
+                          <div className='flex items-center gap-1 px-3 py-1.5 bg-[#15ec5b]/10 border border-[#15ec5b]/30 rounded-md text-sm font-medium group cursor-pointer hover:bg-[#15ec5b]/30 transition-colors'>
+                            Strength Training
+                            <button className='text-[#61896f] hover:text-red-500'>
+                              <i class="ri-close-fill text-[16px]"></i>
+                            </button>
+                          </div>
+
+                          <div className='flex items-center gap-1 px-3 py-1.5 bg-[#15ec5b]/10 border border-[#15ec5b]/30 rounded-md text-sm font-medium group cursor-pointer hover:bg-[#15ec5b]/30 transition-colors'>
+                            Cardio
+                            <button className='text-[#61896f] hover:text-red-500'>
+                              <i class="ri-close-fill text-[16px]"></i>
+                            </button>
+                          </div>
+
+                          <button className='flex items-center gap-1 px-3 py-1.5 bg-slate-50 border border-slate-300 border-dashed rounded-md text-sm font-medium group cursor-pointer hover:text-[#15ec5b] hover:border-[#15ec5b] transition-colors'>
+                            <i class="ri-add-fill text-[16px]"></i>
+                            Add Specialty
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+
+            {/* Right */}
+            <div className='col-span-1 flex flex-col gap-6'>
+              <section className='bg-white rounded-xl border border-[#dbe6df] shadow-sm overflow-hidden'>
+                {/* Heading */}
+                <div className='p-6 border-b border-[#dbe6df] flex items-center justify-between'>
+                  <h3 className='text-lg font-bold flex items-center gap-3'>
+                    <i class="fa-regular fa-bell text-[#15ec5b] text-[20px]"></i>
+                    Notifications
+                  </h3>
+                </div>
+
+                {/* Preferences */}
+                <div className='p-6 flex flex-col gap-6'>
+                  <div className='flex flex-col gap-4'>
+                    <div className='flex items-center justify-between'>
+                      <div className='flex flex-col'>
+                        <span className='text-sm font-medium'>New Bookings</span>
+                        <span className='text-xs text-[#61896f]'>Receive alerts for new sessions.</span>
+                      </div>
+
+                      <label className='relative inline-flex items-center cursor-pointer'>
+                        <input defaultChecked className='sr-only peer' type="checkbox"/>
+                        <div className='w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[""] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border after:border-gray-300 after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#15ec5b]'></div>
+                      </label>
+                    </div>
+
+                    <div className='flex items-center justify-between'>
+                      <div className='flex flex-col'>
+                        <span className='text-sm font-medium'>Class Cancellations</span>
+                        <span className='text-xs text-[#61896f]'>Notify me when a client cancels.</span>
+                      </div>
+
+                      <label className='relative inline-flex items-center cursor-pointer'>
+                        <input defaultChecked className='sr-only peer'type="checkbox" />
+                        <div className='w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[""] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border after:border-gray-300 after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#15ec5b]'></div>
+                      </label>
+                    </div>
+
+                    <div className='flex items-center justify-between'>
+                      <div className='flex flex-col'>
+                        <span className='text-sm font-medium'>Marketing Emails</span>
+                        <span className='text-xs text-[#61896f]'>Receive news and special offers.</span>
+                      </div>
+
+                      <label className='relative inline-flex items-center cursor-pointer'>
+                        <input className='sr-only peer'type="checkbox" />
+                        <div className='w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[""] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border after:border-gray-300 after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[#15ec5b]'></div>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+
+          <footer className='mt-12 text-center text-xs text-[#61896f]'>
+            <p>
+              © 2024 FitPulse Gym Management System. All rights reserved.
+            </p>
+          </footer>
+        </div>
+      </div>
     </div>
   )
 }
--- Frontend/src/pages/trainer/TrainerSupport.jsx
+++ Frontend/src/pages/trainer/TrainerSupport.jsx
@@ -2,8 +2,163 @@ import React from 'react'
 
 const TrainerSupport = () => {
   return (
-    <div>
-      Trainer Support Page
+    <div className=''>
+      <div className='flex-1 overflow-y-auto p-8 scroll-smooth'>
+        <div className='max-w-300 mx-auto space-y-8 pb-10'>
+          {/* Heading */}
+          <div className='flex items-center justify-between gap-4'>
+            <div className='flex flex-col items-start justify-center gap-2'>
+              <h1 className='text-4xl font-black tracking-tight'>Support Center</h1>
+              <p className='text-[#61896f] text-base'>Find answers quickly or get in touch with our team.</p>
+            </div>
+            <div className='w-96 relative group'>
+              <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                <i class="fa-solid fa-magnifying-glass text-gray-400 group-focus-within:text-[#15ec5b] transition-colors"></i>
+              </div>
+              <input className='block w-full pl-10 pr-3 py-3 rounded-xl bg-white shadow-sm ring ring-gray-200 placeholder:text-gray-400 focus:outline-0 focus:ring-2 focus:ring-[#15ec5b] text-sm leading-6 transition-shadow' type="text" placeholder='Search articles, guides, or FAQs...' />
+            </div>
+          </div>
+
+          {/* FAQs and Contact */}
+          <div className='grid grid-cols-3 gap-8 items-start'>
+            {/* Left */}
+            <div className='col-span-2 flex flex-col gap-6'>
+              <div className='flex items-center justify-between'>
+                <h3 className='text-xl font-bold'>Common Questions</h3>
+                <a className='text-sm font-semibold text-[#15ec5b] hover:underline' href="">View all articles</a>
+              </div>
+
+              <div className='flex flex-col gap-4'>
+                <div className='bg-white rounded-xl shadow-sm border border-gray-100 overflow-hidden group'>
+                  <button className='w-full px-6 py-5 flex items-center justify-between text-left focus:outline-none'>
+                    <span className='text-lg font-semibold'>How do I add a new trainer?</span>
+                    <i class="ri-arrow-up-s-line text-gray-400 text-[24px]"></i>
+                  </button>
+                  <div className='px-6 pb-6 pt-2 border-t border-gray-100'>
+                    <p className='text-gray-600 leading-relaxed'>
+                      Go to the
+                      <span className='text-[#15ec5b] font-medium'> Team Settings </span>
+                      page from the sidebar. Click on the "Add New Member" button at the top right corner. You'll need to enter their email address and assign them a role (e.g., Trainer, Admin). They will receive an invitation email to set up their password.
+                    </p>
+                  </div>
+                </div>
+
+                <div className='bg-white rounded-xl shadow-sm border border-gray-100 overflow-hidden group'>
+                  <button className='w-full px-6 py-5 flex items-center justify-between text-left focus:outline-none'>
+                    <span className='text-lg font-semibold'>How do I update my billing information?</span>
+                    <i class="ri-arrow-down-s-line text-gray-400 text-[24px]"></i>
+                  </button>
+                </div>
+
+                <div className='bg-white rounded-xl shadow-sm border border-gray-100 overflow-hidden group'>
+                  <button className='w-full px-6 py-5 flex items-center justify-between text-left focus:outline-none'>
+                    <span className='text-lg font-semibold'>Can I export member data to CSV?</span>
+                    <i class="ri-arrow-down-s-line text-gray-400 text-[24px]"></i>
+                  </button>
+                </div>
+
+                <div className='bg-white rounded-xl shadow-sm border border-gray-100 overflow-hidden group'>
+                  <button className='w-full px-6 py-5 flex items-center justify-between text-left focus:outline-none'>
+                    <span className='text-lg font-semibold'>How do I create a recurring class schedule?</span>
+                    <i class="ri-arrow-down-s-line text-gray-400 text-[24px]"></i>
+                  </button>
+                </div>
+              </div>
+
+              <h3 className='text-lg font-bold mt-4'>Browse Topics</h3>
+
+              <div className='grid grid-cols-3 gap-4'>
+                <a className='bg-white p-4 rounded-xl shadow-sm border border-gray-100 flex flex-col items-center text-center gap-3 hover:shadow-md hover:-translate-y-1 transition-all group' href="">
+                  <div className='size-10 rounded-full bg-blue-50 flex items-center justify-center text-blue-600 group-hover:scale-120 transition-transform'>
+                    <i class="fa-solid fa-money-bills text-[20px]"></i>
+                  </div>
+                  <span className='text-sm font-semibold '>Billing & Plans</span>
+                </a>
+
+                <a className='bg-white p-4 rounded-xl shadow-sm border border-gray-100 flex flex-col items-center text-center gap-3 hover:shadow-md hover:-translate-y-1 transition-all group' href="">
+                  <div className='size-10 rounded-full bg-green-50 flex items-center justify-center text-green-600 group-hover:scale-120 transition-transform'>
+                    <i class="ri-user-settings-line text-[20px]"></i>
+                  </div>
+                  <span className='text-sm font-semibold '>Account Management</span>
+                </a>
+
+                <a className='bg-white p-4 rounded-xl shadow-sm border border-gray-100 flex flex-col items-center text-center gap-3 hover:shadow-md hover:-translate-y-1 transition-all group' href="">
+                  <div className='size-10 rounded-full bg-purple-50 flex items-center justify-center text-purple-600 group-hover:scale-120 transition-transform'>
+                    <i class="ri-webhook-line text-[20px]"></i>
+                  </div>
+                  <span className='text-sm font-semibold '>Integrations</span>
+                </a>
+              </div>
+            </div>
+
+            {/* Right */}
+            <div className='flex flex-col gap-6'>
+              {/* Contact */}
+              <div className='bg-white rounded-2xl shadow-lg shadow-gray-200/50 border border-gray-100 p-8'>
+                <div className='flex items-center gap-3 mb-6'>
+                  <div className='p-2 bg-[#15ec5b]/20 rounded-lg text-green-800'>
+                    <i class="fa-regular fa-envelope text-[20px]"></i>
+                  </div>
+                  <h3 className='text-xl font-bold '>Send us a message</h3>
+                </div>
+
+                <form className='flex flex-col gap-5'>
+                  <div className='flex flex-col gap-2'>
+                    <label className='text-xs font-bold text-gray-500 uppercase tracking-wider'>Subject</label>
+                    <input className='w-full rounded-lg bg-gray-50 px-4 py-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-[#15ec5b]' type="text" placeholder='Brief description of the issue' />
+                  </div>
+
+                  <div className='flex flex-col gap-2'>
+                    <label className='text-xs font-bold text-gray-500 uppercase tracking-wider'>Message</label>
+                    <textarea className='w-full rounded-lg bg-gray-50 px-4 py-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-[#15ec5b] resize-none' placeholder='Describe your issue in detail...' rows='4'></textarea>
+                  </div>
+
+                  <div className='flex flex-col gap-2'>
+                    <label className='text-xs font-bold text-gray-500 uppercase tracking-wider'>Attachments (Optional)</label>
+                    <div className='flex items-center justify-center w-full'>
+                      <label className='flex flex-col items-center justify-center w-full h-24 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 transition-colors' for='dropzone-file'>
+                        <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+                          <i class="fa-solid fa-cloud-arrow-up text-gray-500 mb-1"></i>
+                          <p className='text-xs text-gray-500 '>Click to upload or drag and drop</p>
+                        </div>
+                        <input className='hidden' id='dropzone-file' type="file" />
+                      </label>
+                    </div>
+                  </div>
+
+                  <button className='mt-2 w-full bg-[#15ec5b] hover:bg-green-400 rounded-lg py-3 text-sm font-bold shadow-md shadow-green-400/20 transition-all active:scale-[0.98]'>Submit Request</button>
+                </form>
+              </div>
+
+              {/* Info Widget */}
+              <div className='bg-gray-900 rounded-xl p-6 text-white relative overflow-hidden'>
+                <div className='absolute -top-10 -right-10 size-32 bg-[#15ec5b]/20 rounded-full blur-2xl pointer-events-none'></div>
+
+                <h4 className='text-lg font-bold mb-4 relative z-10'>Direct Support</h4>
+
+                <div className='flex flex-col gap-10 relative z-10'>
+                  <div className='flex items-start gap-3'>
+                    <i class="fa-regular fa-envelope text-[#15ec5b] text-xl mt-0.5"></i>
+                    <div>
+                      <p className='text-xs text-gray-400 font-medium uppercase tracking-wide'>Email Us</p>
+                      <a className='text-sm font-semibold hover:text-[#15ec5b] transition-colors' href="">support@gymsaas.com</a>
+                    </div>
+                  </div>
+
+                  <div className='flex items-start gap-3'>
+                    <i class="fa-regular fa-clock text-[#15ec5b] text-xl mt-0.5"></i>
+                    <div>
+                      <p className='text-xs text-gray-400 font-medium uppercase tracking-wide'>Response Time</p>
+                      <p className='text-sm font-medium'>Usually replies within 24 hours.</p>
+                      <p className='text-xs text-gray-500 mt-1'>Mon-Fri, 9am - 5pm EST</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
