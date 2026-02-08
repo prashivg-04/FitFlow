72102e056a3a7d83be75f8cf7a37d6b2326d52da
prashiv.g04 <prashivgoyal1504@gmail.com>
2026-02-08 11:23:37 +0530

    created axios, called login api, setup redux and established role base navigation

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import 'remixicon/fonts/remixicon.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import store from './store/store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import bgImg from '../media/gymLogin.jpg'
 import navjot from '../media/navjotImg.jpeg'
 import icon from '../media/icon.png'
 import googleLogo from '../media/googleLogo.png'
+import api from '../api/axios'
+import { useDispatch, useSelector } from 'react-redux';
+import { loginFailure, loginStart, loginSuccess } from '../store/authSlice'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if(!isAuthenticated || !user?.role) return;
+      
+    switch(user.role) {
+      case 'OWNER':
+        navigate('/owner/dashboard', { replace: true });
+        break;
+      case 'TRAINER': 
+        navigate('/trainer/join', { replace: true });
+        break;
+      case 'MEMBER':
+        navigate('/member/join', { replace: true });
+        break;  
+    }
+  }, [isAuthenticated, user, navigate]);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    dispatch(loginStart());
+
+    try {
+      const response = await api.post('/auth/login', {
+        email, 
+        password
+      });
+      dispatch(loginSuccess(response.data.data));
+      console.log("Login successfull", response.data);
+    } catch(err) {
+      console.error("Login failed", err);
+      dispatch(loginFailure(err.response?.data?.message || 'Login failed. Please try again.'));
+    }
+  }
+
   return (
     <div className='bg-[#f7f8f6] font-display antialiased min-h-screen'>
       <div className='flex w-full'>
@@ -63,14 +109,20 @@ const Login = () => {
           <div className='w-full flex flex-col max-w-110 gap-6'>
             <div className='flex flex-col gap-2 mb-2'>
               <h1 className='text-slate-900 text-[32px] font-bold leading-tight tracking-tight'>Welcome back</h1>
-              <p className='text-slate-400 text-base font-normal'>Please enter your details to sign in.</p>
+              <p className='text-slate-400 text-base font-normal'>Please enter your details to login.</p>
             </div>
 
-            <form className='flex flex-col gap-5' action="">
+            <form onSubmit={handleLogin} className='flex flex-col gap-5'>
               <div className='flex flex-col gap-1.5'>
                 <label className='text-base font-medium leading-normal' htmlFor="">Email</label>
                 <div className='relative group'>
-                  <input className='form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg border border-[#dbe6df] focus:outline-0 focus:ring-2 focus:ring-[#15ec5b]/50 focus:border-[#15ec5b] h-14 p-4 text-base font-normal leading-none transition-all' type="text" placeholder='john.doe@example.com' />
+                  <input 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className='form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg border border-[#dbe6df] focus:outline-0 focus:ring-2 focus:ring-[#15ec5b]/50 focus:border-[#15ec5b] h-14 p-4 text-base font-normal leading-none transition-all' 
+                    type="text" 
+                    placeholder='john.doe@example.com'
+                  />
                   <i class="ri-mail-line material-symbol-outlined absolute right-4 top-4 group-focus-within:text-[#15ec5b] transition-colors"></i>
                 </div>
               </div>
@@ -78,7 +130,13 @@ const Login = () => {
               <div className='flex flex-col gap-1.5'>
                 <label className='text-base font-medium leading-normal' htmlFor="">Password</label>
                 <div className='relative group'>
-                  <input className='form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg border border-[#dbe6df] focus:outline-0 focus:ring-2 focus:ring-[#15ec5b]/50 focus:border-[#15ec5b] h-14 p-4 text-base font-normal leading-none transition-all' type="password"  placeholder='Min. 8 characters' />
+                  <input 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className='form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg border border-[#dbe6df] focus:outline-0 focus:ring-2 focus:ring-[#15ec5b]/50 focus:border-[#15ec5b] h-14 p-4 text-base font-normal leading-none transition-all' 
+                    type="password"  
+                    placeholder='Min. 8 characters' 
+                  />
                   <i class="ri-eye-line material-symbol-outlined absolute right-4 top-4 group-focus-within:text-[#15ec5b] transition-colors"></i>
                 </div>
               </div>
@@ -88,7 +146,7 @@ const Login = () => {
               </div>
 
               <button className='flex justify-center items-center text-center rounded-lg w-full bg-[#15ec5b] h-14 text-slate-900 text-base font-bold tracking-wide px-5 shadow-lg shadow-[#15ec5b]/20 hover:bg-[#0fdc53] transition-colors'>
-                Sign in
+                Login
               </button>
 
               <div className='relative flex items-center py-2 mt-2'>
@@ -97,10 +155,10 @@ const Login = () => {
                 <div className='grow border-t border-[#dbe6df] '></div>
               </div>
 
-              <button className='flex items-center justify-center bg-white w-full h-14 gap-3 rounded-lg border border-[#dbe6df] px-5 text-[#111813] text-base font-medium hover:bg-gray-50 hover:border-gray-300 transition-colors'>
+              <div className='flex items-center justify-center bg-white w-full h-14 gap-3 rounded-lg border border-[#dbe6df] px-5 text-[#111813] text-base font-medium hover:bg-gray-50 hover:border-gray-300 transition-colors'>
                 <img className='size-6' src={googleLogo} alt="Google Logo" />
-                Sign in with Google
-              </button>
+                Login with Google
+              </div>
             </form>
 
             <div className='text-center mt-4'>
